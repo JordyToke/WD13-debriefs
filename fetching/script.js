bb91b8a6483d9746b9b054a70265808bd5b12e18
JordyToke <jordyn.toke@gmail.com>
2024-11-08 11:35:32 +1100

    implemented API fetching, and passing data to a table on our html

--- fetching/script.js
+++ fetching/script.js
@@ -1,7 +1,5 @@
-
 // fetch function to get book data
-async function getBookData() {
-  const url = "https://fakestoreapi.com/products/1";
+async function getProductData(url) {
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -10,7 +8,41 @@ async function getBookData() {
     // wait for response json to be parsed and print it to console
     const responseJson = await response.json();
     console.log(responseJson);
+    return responseJson;
   } catch (error) {
     console.error(error.message);
   }
 }
+
+// selectors
+const title = document.getElementById('title');
+
+const category = document.getElementById('category');
+
+const price = document.getElementById('price');
+
+const description = document.getElementById('description');
+
+const image = document.getElementById('image');
+
+// put book data to table fields
+const bookData = getProductData('https://fakestoreapi.com/products/1').then(data => {
+  title.innerHTML = data.title;
+  category.innerHTML = data.category;
+  price.innerHTML = '$' + data.price;
+  description.innerHTML = data.description;
+  image.src = data.image;
+});
+
+// put second product data to table fields
+const nextProduct = async () => getProductData('https://fakestoreapi.com/products/2').then(data => {
+  title.innerHTML = data.title;
+  category.innerHTML = data.category;
+  price.innerHTML = '$' + data.price;
+  description.innerHTML = data.description;
+  image.src = data.image;
+});
+
+const nextButton = document.getElementById('nextButton');
+
+nextButton.addEventListener('click', nextProduct);
